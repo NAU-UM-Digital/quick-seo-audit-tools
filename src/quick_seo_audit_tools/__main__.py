7f62376b2f5db3d47665492c6e8565cc82ec7a7f
Geoff Lambeth <geofflambeth@gmail.com>
2023-07-21 13:24:58 -0700

    add basic markdown scrape using sitemap xml

--- src/quick_seo_audit_tools/__main__.py
+++ src/quick_seo_audit_tools/__main__.py
@@ -6,6 +6,9 @@ import csv
 import re
 import lxml
 from urllib.parse import urljoin, urlsplit, urlunsplit
+import os
+import json
+import pandoc
 
 def testAppRequestGet():
     global args
@@ -13,9 +16,9 @@ def testAppRequestGet():
 
 def getLinksStatus():
     global args
-    if args.xml_index is True:
+    if args.xml_index is not False:
         print("beginning sitemap parse...")
-        allSitemaps, allPages = parseInputSitemap(args.destination)
+        allSitemaps, allPages = parseInputSitemap(args.xml_index)
         print("sitemap parse complete, searching for links...")
         allPageStatus = []
         foundUrlsLookup = []
@@ -27,7 +30,7 @@ def getLinksStatus():
         if args.output_csv is not False:
             with open(args.output_csv, 'w') as f:
                 write = csv.writer(f, quoting=csv.QUOTE_ALL, lineterminator='\n')
-                write.writerow(['source URL','found URL','link text','initial response status','X-Redirect-By header', 'redirect chain length', 'destination URL','final response status','final response content type','notes and exception responses'])
+                write.writerow(['source URL','found URL','link text','opens in new tab?','initial response status','X-Redirect-By header', 'redirect chain length', 'destination URL','final response status','final response content type','notes and exception responses'])
 
         combinedPageLookups = matchPagesWithFoundUrls(allPageStatus, foundUrlsLookup)
         cliPrint("ALL PAGES FOUND WITH STATUS")
@@ -38,6 +41,27 @@ def getLinksStatus():
                     write = csv.writer(f, quoting=csv.QUOTE_ALL, lineterminator='\n')
                     write.writerow(page)
 
+    else:
+        print("no supported flags provided—try --help or -h for usage.")
+
+def sitemapScrapeToMarkdown():
+    global args
+    if args.xml_index is not False:
+        print("beginning sitemap parse...")
+        allSitemaps, allPages = parseInputSitemap(args.xml_index)
+        print("sitemap parse complete, scraping pages to markdown...")
+
+        if args.output_folder[-1] != "/":
+            scrape_output_folder = args.output_folder+"/"
+        else:
+            scrape_output_folder = args.output_folder
+
+        for i in allPages:
+            cliPrint("-----", False)
+            scrape_convert_writefile(i, outputPath=scrape_output_folder, getBodyClass=True)
+
+        cliPrint("-----", False)
+        print("scrape complete, pages scraped to markdown at destination: "+scrape_output_folder)
     else:
         print("no supported flags provided—try --help or -h for usage.")
 
@@ -52,22 +76,28 @@ general.add_argument(
     action="store_true",
     help="suppress response text"
 )
+general.add_argument(
+    "--debug",
+    action="store_true",
+    help="show errors on failure"
+)
 
 subparsers = parser.add_subparsers(
-    title="dev or testing"
+    title="cli tools for SEO auditing"
 )
-requestHeaders_parser = subparsers.add_parser("alpha-headers", help="customize request headers with flags")
+requestHeaders_parser = subparsers.add_parser("alpha-headers", help="[ALPHA] customize request headers with flags")
 requestHeaders_parser.add_argument("destination")
 requestHeaders_parser.add_argument("--email", metavar="STRING", action="store", help="add email to response headers", default=False)
 requestHeaders_parser.add_argument("--user-agent", metavar="STRING", action="store", help="customize user-agent in response headers", default=False)
 requestHeaders_parser.set_defaults(func=testAppRequestGet)
 
-linksStatus_parser = subparsers.add_parser("links-status", help="process on-page hyperlinks for response status")
-linksStatus_parser.add_argument("destination")
+linksStatus_parser = subparsers.add_parser("links-status", help="[PROD] process on-page hyperlinks for response status")
 linksStatus_parser.add_argument(
     "--xml-index",
-    action="store_true",
-    help="destination URL returns xml index of pages or other xml indexes"
+    action="store",
+    metavar="URL",
+    help="destination URL returns xml index of pages or other xml indexes",
+    default=False
 )
 linksStatus_parser.add_argument(
     "--output-csv",
@@ -78,6 +108,24 @@ linksStatus_parser.add_argument(
 )
 
 linksStatus_parser.set_defaults(func=getLinksStatus)
+
+markdownScrape_parser = subparsers.add_parser("markdown-scrape", help="[ALPHA] scrape pages from sitemap and output markdown")
+markdownScrape_parser.add_argument(
+    "--xml-index",
+    action="store",
+    metavar="URL",
+    help="destination URL returns xml index of pages or other xml indexes",
+    default=False
+)
+markdownScrape_parser.add_argument(
+    "--output-folder",
+    action="store",
+    metavar="FILE",
+    help="relative folder path for markdown output",
+    default='scrape_markdown/'
+)
+
+markdownScrape_parser.set_defaults(func=sitemapScrapeToMarkdown)
 
 args = parser.parse_args()
 
@@ -233,8 +281,194 @@ def matchPagesWithFoundUrls(urlStatuses, lookupUrls):
                 urlLookup.extend(foundPageStatus[1:])
     return lookupUrls
 
-def main_cli():
+def scrape_convert_writefile(URL, outputPath="scrape_output/", getBodyClass=True):
+
+    scrapeDataDestination = outputPath+"_scrape-data/"
+    if not os.path.exists(scrapeDataDestination):
+        os.makedirs(scrapeDataDestination)
+        cliPrint("Directory "+scrapeDataDestination+" Created ")
+    #else:    
+        #cliPrint("Directory "+scrapeDataDestination+" already exists")
+
+
+    page = requests.get(URL)
+    contentSoup = BeautifulSoup(page.content, "html.parser")
+
+    path = str(URL[:-1].replace("https://", outputPath)+".md")
+    cliPrint("LOCAL PATH: "+path)
+
     try:
-        args.func()
+        title = str(contentSoup.find("title"))
     except:
-        print("try asking for --help")
+        title = "<title>ERROR PULLING ATTRIBUTE FROM NODE</title>"
+    try:
+        description = str("<meta name=\"description\" content=\"")+str(
+            contentSoup.find("meta", {"name": "description"}).get("content"))+str("\"/>")
+    except:
+        description = "<meta name=\"description\" content=\"ERROR PULLING ATTRIBUTE FROM NODE\"/>"
+
+    try:
+        canonicalURL = str(contentSoup.find(
+            "link", {"rel": "canonical"}).get("href"))
+    except:
+        canonicalURL = "ERROR PULLING ATTRIBUTE FROM NODE"
+    cliPrint("CANONICAL URL: "+canonicalURL)
+    for link in contentSoup.find_all('a'):
+        linkStr = ""
+        linkStr = str(link.get("href"))
+        if ("nau.edu" in linkStr) and ("mailto" not in linkStr):
+            logURLs = open(scrapeDataDestination+"urls.csv", "a")
+            logURLs.write(canonicalURL+","+linkStr+"\n")
+            logURLs.close()
+
+    if getBodyClass == True:
+        try:
+            templateStr = str(contentSoup.find("body").get("class"))
+            template = str(
+                "<meta name=\"body-class\" content=\"")+templateStr+str("\"/>\n<meta name=\"canonical\" content=\"")+canonicalURL+str("\">")
+        except:
+            template = str("<meta name=\"body-class\" content=\"ERROR PULLING ATTRIBUTE FROM NODE\"/>\n<meta name=\"canonical\" content=\"")+canonicalURL+str("\">")
+    else:
+        template = "" 
+    try:
+        weirdDivs = contentSoup.findAll(
+            "div", {"class": "panelLinkTypeArrow"})
+        for weirdDiv in weirdDivs:
+            weirdLinks = weirdDiv.findAll("a")
+            for wlink in weirdLinks:
+                wlinkURL = wlink.get("href")
+                wlinkALT = wlink.get("alt")
+                blockUrlTag = str("<a href='")+wlinkURL + \
+                    str("'>")+wlinkALT+str("</a>")
+                urlSoup = BeautifulSoup(blockUrlTag, "html.parser")
+                try:
+                    wlink.append(urlSoup)
+                except:
+                    cliPrint("couldn't find wlink")
+    except:
+        weirdLinks = "No weird NAU21 block links"
+        cliPrint(weirdLinks)
+    
+    try:
+        statsBlocks = [
+            contentSoup.findAll("div", {"class": "triplet-stat"}),
+            contentSoup.findAll("div", {"class": "left-right-stat"})
+        ]
+        for statsBlockSelector in statsBlocks:
+            try:
+                for i in statsBlockSelector:
+                    try:
+                        logIdentifier = "stat"
+                        statContent = i.find("div", {"class": "content"})
+                        try:
+                            statRepoId = 'https://in.nau.edu/repository/wp-admin/post.php?post=' + \
+                                i['data-repository'] + '&action=edit'
+                            try:
+                                statData = requests.get(
+                                    'https://in.nau.edu/repository/wp-json/content-repo/v1/content/statistic/id/'+i['data-repository'])
+                                data = json.loads(statData.content)
+                                statRepoPostStatus = data[0]["post_status"]
+                            except:
+                                statRepoPostStatus = "COULD NOT FIND REPO POST STATUS"
+                        except:
+                            statRepoId = "COULD NOT FIND REPO ID"
+                            statRepoPostStatus = "COULD NOT FIND REPO POST STATUS"
+                        try:
+                            statEyebrow = statContent.find("div", {"class": "eyebrow-underline"}).get_text().strip()
+                        except:
+                            statEyebrow = ""
+                        try:
+                            statNum = statContent.find("div", {"class": "number"}).get_text().strip()
+                        except:
+                            statNum = ""
+                        try:
+
+                            statLabel = statContent.find("div", {"class": "label"}).get_text().strip()
+                        except:
+                            statLabel = ""
+                        try:
+                            statPub = statContent.find("div", {"class": "publication"}).get_text().strip()
+                        except:
+                            statPub = ""
+                        allStatsList = [str(statRepoId), str(statRepoPostStatus), str(statEyebrow), str(statNum), str(statLabel), str(statPub), str(logIdentifier), str(canonicalURL)]
+                        cliPrint(allStatsList)
+                        with open(scrapeDataDestination+'stats.csv', 'a') as f:
+                            write = csv.writer(f, quoting=csv.QUOTE_ALL, lineterminator='\n')
+                            write.writerow(allStatsList)
+                    except:
+                        cliPrint("\n\n\n\n HAD AN ISSUE PULLING SINGLE STAT")
+                        pass
+            except:
+                cliPrint("\n\n\n\n HAD AN ISSUE PULLING ALL STATS ON PAGE")
+                pass
+    except:
+        pass
+
+    try:
+        tagsToRemove = [
+            contentSoup.findAll("head"),
+            contentSoup.findAll("noscript"),
+            contentSoup.findAll("div", {"id": "top-nav-wrapper"}),
+            contentSoup.findAll("div", {"id": "simple-page-footer"}),
+            contentSoup.findAll("div", {"id": "program-of-interest-mobile"}),
+            contentSoup.findAll("div", {"id": "left-nav-menu"}),
+            contentSoup.findAll("div", {"class": "ginput_container"}),
+            contentSoup.findAll("div", {"class": "ginput_recaptcha"}),
+            contentSoup.findAll("div", {"class": "gfield"}),
+            contentSoup.findAll("div", {"class": "event-container"}),
+            contentSoup.findAll("span", {"class": "screen-reader-text"}),
+            contentSoup.findAll("div", {"class": "screen-reader-text"}),
+            contentSoup.findAll("li", {"class": "gfield"}),
+            contentSoup.findAll("a", {"id": "request-info"}),
+            contentSoup.findAll("footer"),
+            contentSoup.findAll("iframe"),
+        ]
+        try:
+            for foundAll in tagsToRemove:
+                try:
+                    for tag in foundAll:
+                        try:
+                            tag.decompose()
+                        except:
+                            pass
+                except:
+                    pass
+        except:
+            pass
+        body = str(contentSoup)
+
+        logBody = open(scrapeDataDestination+"body.html", "w")
+        logBody.write(body)
+        logBody.close()
+    except:
+        body = str(contentSoup.findAll("div", {"class": "entry-content"}))
+    head = "<head>"+title+description+template+"</head>"
+    html = head+body
+
+    dir = path.rsplit('/', 1)[0]
+
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+        cliPrint("Directory "+dir+" Created ")
+    else:    
+        cliPrint("Directory "+dir+" already exists")
+    
+
+    cliPrint("BODY GOOD")
+    log = open(scrapeDataDestination+"log.csv", "a")
+    log.write(URL+","+canonicalURL+",GOOD BODY\n")
+    log.close()
+
+
+    pandocInput = pandoc.read(html, format="html")
+    pandocOutput = pandoc.write(pandocInput, format="markdown_strict-raw_html+simple_tables+yaml_metadata_block", options=["--wrap=none", "-s"], file=path)
+    return pandocOutput
+
+def main_cli():
+    if args.debug is True:
+        args.func()
+    else:
+        try:
+            args.func()
+        except:
+            print("try asking for --help")
