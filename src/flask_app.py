14f41f75f8ecf4dfa6757c9a759ecd5c872d04a9
Geoffrey Lambeth <geoffrey@geofflambeth.com>
2024-07-29 03:13:04 +0000

    really need to work on that db selection situation

--- src/flask_app.py
+++ src/flask_app.py
@@ -7,7 +7,7 @@ from flask import render_template
 
 
 app = Flask(__name__)
-db_path = f'../test_folder/2024-07-27_crawl-database.db' # REMEMBER TO CHANGE LATER
+db_path = f'../test_folder/2024-07-28_crawl-database.db' # TODO:EXPAND ON DB SELECTION LOGIC 
 db.init_output_db(db_path)
 
 @app.route("/")
